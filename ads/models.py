42a76590bba93e54438c651ab4591483817ad273
Maksim Gostev <teranys1986@gmail.com>
2023-05-09 10:08:22 +0300

    сделал модели

--- ads/models.py
+++ ads/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
+from users.models import User
+
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
-
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
 
-class Ad(mo
+class Ad(models.Model):
+    name = models.CharField(max_length=20)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    price = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, null=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
