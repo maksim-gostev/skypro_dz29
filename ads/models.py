226d0d485d16088c8f1af622c540551bb04431ef
flood <teranys1986@gmail.com>
2023-05-09 10:04:49 +0300

    начало работы

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Ad(mo
