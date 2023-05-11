c0974d4e155271ae072688f3f65c5500c99dce1d
Maksim Gostev <teranys1986@gmail.com>
2023-05-11 14:39:32 +0300

    вроде всё сделал

--- ads/ad/views.py
+++ ads/ad/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -11,6 +12,47 @@ from ads.ad.serializers import AdSerializer, AdCreateSerializer, AdUpdateSeriali
 class AdListView(ListAPIView):
     queryset = Ad.objects.all().order_by('-price')
     serializer_class = AdSerializer
+
+    def get(self, request, *args, **kwargs):
+        text_search = request.GET.get('text', None)
+
+        if text_search:
+            self.queryset = self.queryset.filter(
+                name__icontains=text_search
+            )
+
+        categoryes_search = request.GET.getlist('cat', None)
+        cat_q = None
+        for cat in categoryes_search:
+            if cat_q is None:
+                cat_q = Q(category_id=cat)
+            else:
+                cat_q |= Q(category_id=cat)
+        if cat_q:
+            self.queryset = self.queryset.filter(cat_q)
+
+        location_search = request.GET.get('location', None)
+
+        if location_search:
+            self.queryset = self.queryset.filter(
+                author__location__name__icontains=location_search
+            )
+
+        price_from = request.GET.get('price_from', None)
+
+        if price_from:
+            self.queryset = self.queryset.filter(
+                price__gte=price_from
+            )
+
+        price_to = request.GET.get('price_to', None)
+
+        if price_to:
+            self.queryset = self.queryset.filter(
+                price__lte=price_to
+            )
+
+        return super().get(request, *args, **kwargs)
 
 
 class AdDetailView(RetrieveAPIView):
